a4d748b893f34d38fce765ea0fa6f6a19f626fe9
Truyen Nguyen <truyennv888@gmail.com>
2024-11-05 15:30:32 +0700

    chap 9: Stream, Suspense, Group Component (Suspense for items)

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -71,6 +71,8 @@ export async function fetchCardData() {
     const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
 
+    await new Promise(resolve => setTimeout(resolve, 3000))
+
     return {
       numberOfCustomers,
       numberOfInvoices,
