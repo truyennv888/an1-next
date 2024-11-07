185d62d4632a25a77e03bb8a51c997670c204b87
Truyen Nguyen <truyennv888@gmail.com>
2024-11-07 18:38:54 +0700

    chap 13. Handle error. 404 error

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,24 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page(props: {
-    params: Promise<{
-        id: string,
-    }>;
+  params: Promise<{
+    id: string,
+  }>;
 }) {
-    const params = await props.params;
-    const { id } = params;
-    const [ invoice, customers ] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-    ])
-    
+  const params = await props.params;
+  const { id } = params;
+  const [ invoice, customers ] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ])
+
+  if(!invoice){
+    notFound();
+  }
+  
   return (
     <main>
       <Breadcrumbs
